d0ae171b911aa7f9fb16596c4c83fb7aa03b36ee
Mario <mario.tawfelis@outlook.com>
2024-02-11 18:57:49 +0200

    Setup interfaces and types

---
+++ FlightTracker/src/services/diaryService.ts
@@ -0,0 +1,17 @@
+import diaries from '../../data/entries';
+import { DiaryEntry } from '../types';
+
+const getEntries = (): DiaryEntry[] => {
+  return diaries;
+};
+
+const addDiary = () => {
+  return null;
+};
+
+export default {
+  getEntries,
+  addDiary
+};
+
+
