2ea6f55b975cedd9f852b7a31259518b9d969284
Mario <mario.tawfelis@outlook.com>
2024-02-15 13:57:18 +0200

    Implemented route and service to retrieve Diagonses

--- Patientor/backend/index.ts
+++ Patientor/backend/index.ts
@@ -1,12 +1,16 @@
 import express from 'express';
+import diagnosisRouter from './src/routes/diagnoses';
+
 const app = express();
 
-const PORT = 3003
+const PORT = 3003;
 
 app.get('/api/ping', (_req, res) => {
   res.send('Pong');
 });
 
+app.use('/api/diagnoses', diagnosisRouter);
+
 app.listen(PORT, () => {
   console.log(`Backend is listening on port ${PORT}`);
-})
+});
