9bb2d88e4ca818565d6985b87f96784ed7ef2f24
Mario <mario.tawfelis@outlook.com>
2024-02-22 17:29:09 +0200

    Created a POST endpoint

--- Patientor/backend/src/routes/patients.ts
+++ Patientor/backend/src/routes/patients.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import patientService from '../services/patientService';
+import toNewPatientEntry from "../utils";
 
 const router = express.Router();
 
@@ -7,5 +8,19 @@ router.get('/', (_req, res) => {
   res.send(patientService.getNonSensitivePatientData());
 });
 
-export default router;
+router.post('/', (req, res) => {
+  try {
+    const newEntry = toNewPatientEntry(req.body);
 
+    const addEntry = patientService.addPatient(newEntry);
+    res.json(addEntry);
+  } catch ( error: unknown ) {
+    let errorMessage = 'Something bad happened. ';
+    if (error instanceof Error) {
+      errorMessage += 'Error! ' + error.message;
+    }
+    res.status(400).send(errorMessage);
+  }
+});
+
+export default router;
